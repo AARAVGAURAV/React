c848ddff16cd54afb0cea2bc8690b0fbe37bd598
GauravKumarDhiman <139962975+AARAVGAURAV@users.noreply.github.com>
2024-09-25 18:35:54 +0000

    react1

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+  <h2>chai pi lo</h2>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <h1>hello subhe cuhbe 2 mp ka camrea</h1>
+  )
+}
+
+export default App
