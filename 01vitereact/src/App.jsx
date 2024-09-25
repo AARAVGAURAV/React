c69fec77f6a8ee810a42dc2b30f47fe56bfffeae
GauravKumarDhiman <139962975+AARAVGAURAV@users.noreply.github.com>
2024-09-25 19:13:03 +0000

    recact2

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 
-
+import Chai from "./chai"
 function App() {
 
 
   return (
-    <h1>hello subhe cuhbe 2 mp ka camrea</h1>
+    <Chai/>
+    
   )
 }
 
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,14 @@
+function Chai(){
+    return(
+
+        <>
+
+        <h2>chai le lo</h2>
+        <h1>hello</h1>
+        <h2>hellondn</h2>
+        </>
+        
+    )
+}
+
+export default Chai
