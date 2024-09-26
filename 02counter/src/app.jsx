3d6f70c68ebb71412ad1564cf928d341b57246e7
GauravKumarDhiman <139962975+AARAVGAURAV@users.noreply.github.com>
2024-09-26 17:58:14 +0000

    hook

--- 02counter/src/app.jsx
+++ 02counter/src/app.jsx
@@ -4,36 +4,25 @@ import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
-  // hooks to handling multiple references 
-let [counter,setcounter] = useState(15)  
-
-
+  
+const [counter,setcounter] =useState  
 // let counter = 15;
 
-const addValue = () => {
-  console.log("clicked", setcounter);
-  if (setcounter < 20) { // Check if counter is less than 20
-    setcounter+1; // Increment the counter
-  }
-};
-
-
-const removeValue = () => {
-  if (setcounter > 0) { 
-    setcounter - 1 ; 
-  }
-
-
+const addvalue =() => {
+  console.log("clicked",counter);
+  // counter=counter+1
+  
 }
+
   return (
     <>
    <h1>chai aur react</h1>
 
    <h2>Counter Value:{counter}</h2>
 
-   <button onClick={addValue}>Add value</button>
+   <button onClick={addvalue}>Add value</button>
    <br/>
-   <button onClick={removeValue}>Remove value</button>
+   <button>Remove value</button>
     </>
   )
 }
